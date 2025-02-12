4f2947ddd47e0bf911ead2a350640738537245d3
Suman01-tatva <tatva.pci93@outlook.com>
2025-02-12 17:54:06 +0530

    Created dotnet app with DB Connection , Views and Controllers

---
+++ Models/RolePermission.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaShop.Models;
+
+public partial class RolePermission
+{
+    public int Id { get; set; }
+
+    public int PermissionId { get; set; }
+
+    public int RoleId { get; set; }
+
+    public bool? CanView { get; set; }
+
+    public bool? CanEdit { get; set; }
+
+    public bool? CanDelete { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public int CreatedBy { get; set; }
+
+    public DateTime? ModifiedAt { get; set; }
+
+    public int? ModifiedBy { get; set; }
+
+    public virtual User CreatedByNavigation { get; set; } = null!;
+
+    public virtual User? ModifiedByNavigation { get; set; }
+
+    public virtual Role Role { get; set; } = null!;
+}
---
+++ Models/State.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaShop.Models;
+
+public partial class State
+{
+    public int Id { get; set; }
+
+    public int CountryId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<City> Cities { get; set; } = new List<City>();
+
+    public virtual Country Country { get; set; } = null!;
+}
