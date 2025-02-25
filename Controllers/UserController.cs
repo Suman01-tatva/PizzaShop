1e9cbf3ece34135bd9511ac2576534c1b36e4868
Suman01-tatva <tatva.pci93@outlook.com>
2025-02-25 15:31:20 +0530

    Completed User CRUD functionality

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,11 +1,13 @@
 using System.Diagnostics;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using pizzashop.Models;
 using PizzaShop.Models;
 using PizzaShop.Utils;
 using PizzaShop.ViewModels;
@@ -56,7 +58,6 @@ public class UserController : Controller
             ViewData["email"] = email;
             ViewData["FirstName"] = user?.FirstName;
             ViewData["LastName"] = user?.LastName;
-            // Console.WriteLine("Data " + AllStates[1].Name);
             Console.WriteLine("Data " + user?.Address);
             Console.WriteLine("Data " + user?.Email);
             var userViewModel = new ProfileViewModel
@@ -73,24 +74,6 @@ public class UserController : Controller
             };
             return View(userViewModel);
         }
-    }
-
-    [HttpGet]
-    public async Task<IActionResult> GetStates(int countryId)
-    {
-        var states = await _context.States
-            .Where(s => s.CountryId == countryId)
-            .ToListAsync();
-        return Json(states);
-    }
-
-    [HttpGet]
-    public async Task<IActionResult> GetCities(int stateId)
-    {
-        var cities = await _context.Cities
-            .Where(c => c.StateId == stateId)
-            .ToListAsync();
-        return Json(cities);
     }
 
     [HttpPost]
@@ -124,9 +107,6 @@ public class UserController : Controller
             ViewBag.AllCities = new SelectList(await _context.States.Where(s => s.CountryId == user.CountryId).ToListAsync(), "Id", "Name", user.StateId);
             ViewBag.AllStates = new SelectList(await _context.Cities.Where(ct => ct.StateId == user.StateId).ToListAsync(), "Id", "Name", user.CityId);
 
-            // ViewBag.AllCountries = AllCountries;
-            // ViewBag.AllCities = AllCities;
-            // ViewBag.AllStates = AllStates;
             ViewData["ProfileSuccessMessage"] = "Profile Updated SucessFully";
             ViewData["role"] = role;
             ViewData["email"] = email;
@@ -183,7 +163,7 @@ public class UserController : Controller
         .Take(pageSize)
         .ToList();
 
-        var count = _context.Users.Count();
+        var count = _context.Users.Where(u => u.IsDeleted == false).Count();
         ViewBag.count = count;
         ViewBag.pageIndex = pageIndex;
         ViewBag.pageSize = pageSize;
@@ -234,11 +214,14 @@ public class UserController : Controller
 
     public IActionResult CreateUser()
     {
+        ViewBag.Countries = new SelectList(_context.Countries, "Id", "Name");
+        ViewBag.Roles = new SelectList(_context.Roles, "Id", "Name");
         return View();
     }
 
     [HttpPost]
-    public async Task<IActionResult> CreateUser(User model)
+    // [ValidateAntiForgeryToken]
+    public async Task<IActionResult> CreateUser(UserViewModel model)
     {
         if (ModelState.IsValid)
         {
@@ -249,42 +232,184 @@ public class UserController : Controller
                 return View();
             }
 
-            user = new User
+            var currentUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == Request.Cookies["email"]);
+            var hashPassword = PasswordUtills.HashPassword(model.Password);
+            var newUser = new User
             {
                 FirstName = model.FirstName,
                 LastName = model.LastName,
-                Email = model.Email,
                 Username = model.Username,
                 Phone = model.Phone,
-                RoleId = model.RoleId,
-                ProfileImage = model.ProfileImage,
-                Zipcode = model.Zipcode,
-                Address = model.Address,
                 CountryId = model.CountryId,
                 StateId = model.StateId,
                 CityId = model.CityId,
-                CreatedBy = user.Id,
+                Address = model.Address,
+                Zipcode = model.Zipcode,
+                RoleId = model.RoleId,
+                ProfileImage = model.ProfileImage,
+                Email = model.Email,
+                Password = hashPassword,
+                CreatedBy = currentUser.Id
             };
 
-            _context.Users.Add(user);
-            var rowsAffected = await _context.SaveChangesAsync();
-            if (rowsAffected > 0)
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+
+
+
+            var newAccount = new Account
             {
-                Console.WriteLine("User created successfully.");
-                TempData["SuccessMessage"] = "Profile created successfully.";
-                return RedirectToAction(nameof(UserList));
-            }
-            else
-            {
-                Console.WriteLine("No changes detected.");
-                TempData["ErrorMessage"] = "No changes were made.";
-            }
-            return View();
+                RoleId = model.RoleId,
+                Email = model.Email,
+                Password = hashPassword,
+                CreatedBy = currentUser.Id,
+            };
+            _context.Accounts.Add(newAccount);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("UserList");
         }
-        else
-        {
-            return View();
-        }
+        return View(model);
     }
 
+    private async Task LoadDropdowns(UserViewModel model)
+    {
+        ViewBag.Countries = new SelectList(await _context.Countries.ToListAsync(), "Id", "Name", model.CountryId);
+        ViewBag.States = new SelectList(await _context.States.Where(s => s.CountryId == model.CountryId).ToListAsync(), "Id", "Name", model.StateId);
+        ViewBag.Cities = new SelectList(await _context.Cities.Where(c => c.StateId == model.StateId).ToListAsync(), "Id", "Name", model.CityId);
+    }
+
+    [HttpGet]
+    public JsonResult GetStates(int countryId)
+    {
+        var states = _context.States
+            .Where(s => s.CountryId == countryId)
+            .Select(s => new
+            {
+                id = s.Id,
+                name = s.Name
+            }).ToList();
+        return Json(states);
+    }
+
+    [HttpGet]
+    public JsonResult GetCities(int stateId)
+    {
+        var cities = _context.Cities
+            .Where(c => c.StateId == stateId)
+             .Select(c => new
+             {
+                 id = c.Id,
+                 name = c.Name
+             }).ToList();
+        return Json(cities);
+    }
+
+    public async Task<IActionResult> UpdateUser(int id)
+    {
+        ViewBag.Roles = new SelectList(_context.Roles, "Id", "Name");
+
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        ViewBag.Countries = new SelectList(_context.Countries, "Id", "Name", user.CountryId);
+
+        var model = new UserViewModel
+        {
+            Id = user.Id,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Username = user.Username,
+            Phone = user.Phone,
+            CountryId = user.CountryId,
+            StateId = user.StateId,
+            CityId = user.CityId,
+            Address = user.Address,
+            Zipcode = user.Zipcode,
+            RoleId = user.RoleId,
+            ProfileImage = user.ProfileImage,
+            Email = user.Email,
+            Password = user.Password,
+            IsActive = user.IsActive,
+        };
+
+        await LoadDropdowns(model);
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UpdateUser(UserViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = await _context.Users.FindAsync(model.Id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var currentUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == Request.Cookies["email"]);
+
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            user.Username = model.Username;
+            user.Phone = model.Phone;
+            user.CountryId = model.CountryId;
+            user.StateId = model.StateId;
+            user.CityId = model.CityId;
+            user.Address = model.Address;
+            user.Zipcode = model.Zipcode;
+            user.RoleId = model.RoleId;
+            user.ProfileImage = model.ProfileImage;
+            user.Email = model.Email;
+            user.Password = model.Password;
+
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+
+            var selectedAccount = await _context.Accounts.FirstOrDefaultAsync(a => a.Email == model.Email);
+            if (selectedAccount == null)
+            {
+                ViewBag.ErrorMessage = "This user doesn't Exist";
+                return View();
+            }
+            selectedAccount.RoleId = model.RoleId;
+            selectedAccount.Email = model.Email;
+
+            _context.Accounts.Update(selectedAccount);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("UserList");
+        }
+
+        await LoadDropdowns(model);
+        return View(model);
+    }
+
+    [HttpPost]
+    // [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        user.IsDeleted = true;
+
+        try
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+            TempData["SuccessMessage"] = "User deleted successfully.";
+        }
+        catch (Exception ex)
+        {
+            TempData["ErrorMessage"] = "Error deleting user: " + ex.Message;
+        }
+
+        return RedirectToAction("UserList");
+    }
 }
